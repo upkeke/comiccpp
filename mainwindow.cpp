7f5f0c83e81a63bd05811bacfcf36016e462923f
upkeke <storepy@qq.com>
2022-04-17 23:43:34 +0800

    test

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -3,7 +3,7 @@
 #include<vector>
 #include<iostream>
 
-
+#pragma execution_character_set("utf-8")
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
@@ -12,7 +12,7 @@ MainWindow::MainWindow(QWidget *parent)
     //setWindowFlags(Qt::FramelessWindowHint);
     ui->dockWidget->setWindowFlags(Qt::FramelessWindowHint);
 
-    init_graphics();  //初始化 grap
+    init_ui();  //初始化 ui,和一些成员
     get_comic_dir(); //获得路径
     paint_img();  //添加图片
 
@@ -23,14 +23,28 @@ MainWindow::MainWindow(QWidget *parent)
         //QString xxx = "漫画名 "+"当前页"+"/总页数";
         ui->statusbar->showMessage(file_list[num]);
     });
-    connect(ui->pushButton_4,&QPushButton::clicked,this,[&](){
-        qDebug()<<"滚轮顶端y坐标"<<view->verticalScrollBar()->value() ;
-        qDebug()<<"滚轮左端端x坐标"<<view->horizontalScrollBar()->value();
-        qDebug()<<"000000";
-        //qDebug()<<"视图中心的坐标"<<(view->mapToScene(view->sceneRect())).x();
+    connect(ui->pushButton_4,&QPushButton::clicked,this,&MainWindow::get_item);
+
+    //设置 "设置"图标的点击事件
+    connect(ui->actionset_dock,&QAction::triggered,this,[&](){
+        if(!isShow) //没有显示，则显示
+        {
+        ui->dockWidget->show();
+        isShow=true;
+        }
+        else
+        {
+            ui->dockWidget->hide();
+            isShow=false;
+        }
+    });
+    //
+    connect(ui->actionopen,&QAction::triggered,this,[&](){
+        ui->toolBox_2->hide();
     });
 
 }
+
 
 MainWindow::~MainWindow()
 {
@@ -44,10 +58,18 @@ void MainWindow::test_core()
     qDebug()<<ls1[2];
 }
 
-void MainWindow::init_graphics()
+void MainWindow::init_ui()
 {
+    isShow=false;
+    myDock1=new QDockWidget(this,Qt::FramelessWindowHint);
+    addDockWidget(Qt::RightDockWidgetArea,myDock1);
+    myDock1->hide();
+    //myDock1->setWindowFlag(Qt::RightDockWidgetArea);
+    ui->dockWidget->hide();
+    ui->dockWidget->setAllowedAreas(Qt::RightDockWidgetArea);//设置只允许靠右
     scene = new QGraphicsScene(this);
     view = new QGraphicsView(this);
+
 }
 
 void MainWindow::get_comic_dir(QString path)
@@ -67,10 +89,10 @@ void MainWindow::paint_img()
     int w,h;
     QPixmap pix(file_list[0]);
     w=pix.width();
-    h=pix.height();
+    //h=pix.height();
     resize(w,500);
     h=0;
-    for(int i=0;i<20;++i)
+    for(int i=0;i<3;++i)
     {
         QPixmap pix(file_list[i]);
         qDebug()<<file_list[i];
@@ -93,24 +115,36 @@ void MainWindow::paint_img()
     view->setScene(scene);
     //view->centerOn(this->width()/2,this->height()/2);
     view->centerOn(0,0);//场景的的0，0绝对到不了视图的中心，但是这样可以避免菜单工具栏的误差，保证场景视图左上角重合
-    setCentralWidget(view);
+    setCentralWidget(ui->widget);
 }
 
-void MainWindow::test_img()
+void MainWindow::get_item()
 {
-    resize(1200,1600);
-    QPixmap pix1(file_list[0]);
-    QPixmap pix2(file_list[1]);
-    QGraphicsPixmapItem * pixitem1 = new QGraphicsPixmapItem(pix1);
-    QGraphicsPixmapItem * pixitem2 = new QGraphicsPixmapItem(pix2);
-    pixitem1->setPos(0,0);
-    pixitem2->setPos(0,1600);
-    scene->addItem(pixitem1);
-    scene->addItem(pixitem2);
-    view->setScene(scene);
-    //view->centerOn(0,0);
-    view->centerOn(this->width()/2,this->height()/2);
-    setCentralWidget(view);
+    int y=view->verticalScrollBar()->value();
+    //auto item=view->itemAt(100,y);
+    auto item=view->itemAt(100,100);
+    QGraphicsPixmapItem *pixitem=static_cast<QGraphicsPixmapItem*>(item);
+    qDebug()<<"当前页数是"<<grp_pix_list.indexOf(pixitem);
+    qDebug()<<"图元的尺寸"<<pixitem->pixmap().size();
+    qDebug()<<"滚轮顶端y坐标"<<y ;
+    qDebug()<<"滚轮左端端x坐标"<<view->horizontalScrollBar()->value();
+    QString str =QString("当前页数是%1,图元的尺寸%2,%3")
+            .arg(grp_pix_list.indexOf(pixitem))
+            .arg(pixitem->pixmap().size().width())
+            .arg(pixitem->pixmap().size().height());
+    ui->statusbar->showMessage(str);
+
 }
+
+void MainWindow::mousePressEvent(QMouseEvent *event)
+{
+    if(isShow)
+    {
+        ui->dockWidget->hide();
+        isShow=false;
+    }
+
+}
+
 
 
